cfc9d3a5b77f97561ec4378eedd2490050cea641
Александр Немеров <nemerov_a@mail.ru>
2022-12-06 20:22:06 +0800

    21356465

--- Lab_8_OOP/Task2/Character.cs
+++ Lab_8_OOP/Task2/Character.cs
@@ -34,17 +34,6 @@ namespace Lab_8_OOP.Task2
 
         bool IRole.Alive => HP > 0;
 
-        //int IRole.Damage
-        //{
-        //    get
-        //    {
-        //        if (this.Role == Role.warrior) return ((IWarrior)this).Damage;
-        //        if (this.Role == Role.arrcher) return ((IArcher)this).Damage;
-        //        if (this.Role == Role.mage) return ((IMage)this).Damage;
-        //        return 0;
-        //    }
-        //}
-
         void IRole.getDamage(int damage)
         {
             this.HP -= damage;
